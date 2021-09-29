6435939c0868a6e99fb8488938406e51b7946233
djwagner1 <djwagner1@buffs.wtamu.edu>
2021-09-29 13:42:18 -0500

    update

---
+++ Assignment_chapter_4/app.py
@@ -0,0 +1,23 @@
+# David Wagner MSFE - CIDM 6303
+# Add your code here according to the instructions
+# Chapter 4.1 Defining functions
+
+def greet():
+    print("Hi there")
+    print("Welcome Aboard")
+greet()
+print("End Chapter 4.1 Defining Functions\n")
+
+# Chapter 4.2 Arguments. This chapter demonstrate two arguments
+# Note. You cannot have functions with the same name in the same file,
+# which is why we change the name to greet2().
+
+def greet2(first_name, last_name):
+    print("Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+greet2("Ada", "Lovelace")
+print("*** End of Chapter 4.2 Arguments\n")
+
+
+
