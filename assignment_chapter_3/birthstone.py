a0b568be55142e7b8cf36b68f016a4de8aaf3713
djwagner1 <djwagner1@buffs.wtamu.edu>
2021-09-29 10:02:56 -0500

    update

--- assignment_chapter_3/birthstone.py
+++ assignment_chapter_3/birthstone.py
@@ -1,33 +1,35 @@
 # David Wagner MSFE CIDM 6303 Assignment 3 pt 2
 # Follow the assignment instructions to code an app that
 # will tell a user their birthstone.
-try:
-    user_input = input("Enter the number of your birth month:")
-    month = int(user_input)
-    print(month)
-    if month == 1:
+
+user_input = input("Enter the number of your birth month:")
+month = int(user_input)
+print(month)
+if month == 1:
         print("Your birthstone is Garnet")
-    elif month == 2:
+elif month == 2:
         print("Your birthstone is Amethyst")
-    elif month == 3:
+elif month == 3:
         print("Your birthstone is Aquamarine")
-    elif month == 4:
+elif month == 4:
         print("Your birthstone is Diamond")
-    elif month == 5:
+elif month == 5:
         print("Your birthstone is Emerald")
-    elif month == 6:
+elif month == 6:
         print("Your birthstone is Alexandrite")
-    elif month == 7:
+elif month == 7:
         print("Your birthstone is Ruby")
-    elif month == 8:
+elif month == 8:
         print("Your birthstone is Peridot")
-    elif month == 9:
+elif month == 9:
         print("Your birthstone is Sapphire")
-    elif month == 10:
+elif month == 10:
         print("Your birthstone is Pink Tourmaline")
-    elif month == 11:
+elif month == 11:
         print("Your birthstone is Topaz")
-    elif month == 12:
-        print("Your birthstone is Blue Topaz")   
-except ValueError:
-    print("Not a Valid entry. Try a number 1-12")
+elif month == 12:
+        print("Your birthstone is Blue Topaz") 
+else:
+        print("the number must 1-12")
+        
+
