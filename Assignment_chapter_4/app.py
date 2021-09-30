376786244a10e400fcf88a5bb8db94f355b07b01
djwagner1 <djwagner1@buffs.wtamu.edu>
2021-09-30 09:52:50 -0500

    update 3

--- Assignment_chapter_4/app.py
+++ Assignment_chapter_4/app.py
@@ -104,3 +104,18 @@ print("***End of Chapter 4.8 Scope\n")
 #Watch the video and learn how to debug. Nothing to code.
 
 
+# Chapter 4.12 Exercise / 4.13 Solution
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+    print(fizz_buzz(7))
+    print(fizz_buzz(3))
+    print(fizz_buzz(5))
+    print("***End Chapter 4.12 Exercise / 4.13 Solution")
+    
--- Assignment_chapter_4/sandbox.py
+++ Assignment_chapter_4/sandbox.py
@@ -1,65 +1,13 @@
-def greet2(first_name, last_name):
-    print(f"Hi {first_name} {last_name}")
-    print("Welcome Aboard")
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
 
-greet2("Ada", "Lovelace") #Google who Ada Lovelace was
-print("***End Chapter 4.2 Arguments\n")
-
-#Chapter 4.3 Types of Functions
-#Function of 'Do Tasks'
-def greet3(name):
-    print(f"Hi {name}")
-
-print(greet3("Ada"))
-
-# Function of 'return value'
-def greet4(name):
-    return(f"Hi {name}")
-
-message = greet4("Sophie Wilson")
-print(message)
-message = greet4("Grace Hopper")
-print(message)
-message = greet4("Ida Rhodes")
-print(message)
-print("***End of Chapter 4.3 Types of Functions\n")
-
-# Chapter 4.4 Keyword Arguments
-def increment(number, by):
-    return number + by
-
-print(increment(2, by=1))
-print("***End Chapter 4.4 Keyword Arguments\n")
-
-# Chapter 4.5 Default Arguments
-def increment_default(number, by=1):
-    return number + by
-
-print(increment_default(2,5))
-print("***End of Chpater 4.5 Default Arguments\n")
-
-#Chapter 4.6 Xargs
-def mulitply(*numbers):
-    total = 1
-    for number in numbers:
-        total *= number
-    return total
-
-print(mulitply(2,3,4,5))
-print(mulitply(55,87))
-print(mulitply(100,98,77,4,-5,72,0.5))
-print("***End Chapter 4.6 Xargs\n")
-
-#Chapter 4.7 xxargs
-def save_user(**user):
-    print(user["name"])
-
-save_user(id=1,name="John", age=22)
-print("***End of Chapter 4.7 xxargs\n")
-
-#Chapter 4.9 Debugging
-#Watch the video and learn how to debug. Nothing to code.
-
-#Chapters 4.10 & 4.11 VSCode Coding Tricks
-#Watch the video and learn how to debug. Nothing to code.
-
+    print(fizz_buzz(7))
+    print(fizz_buzz(3))
+    print(fizz_buzz(5))
+    print("***End Chapter 4.12 Exercise / 4.13 Solution")
