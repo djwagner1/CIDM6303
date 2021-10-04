614b1f8ee31f77565ec54f1ee4ff2d7612e760d3
djwagner1 <djwagner1@buffs.wtamu.edu>
2021-10-04 14:57:14 -0500

    update 1

---
+++ Assignment_chapter_5/app.py
@@ -0,0 +1,15 @@
+# David Wagner -- CIDM 6303 -- Assign 5
+# Edit this file according to the instructions
+# Import various modules needed for this lesson
+from random import random
+from array import array
+from collections import deque
+# start coding below this line
+
+# Chapter 5.1 Lists
+print("Chapter 5.1 Lists" + "-" *20)
+letters = ["a","b","c"]
+matrix = [[0,1],[2,3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
---
+++ Assignment_chapter_5/app2.py
@@ -0,0 +1,6 @@
+# Edit this file according to the instructions
+# Import various modules needed for this lesson
+from random import random
+from array import array
+from collections import deque
+# start coding below this line
